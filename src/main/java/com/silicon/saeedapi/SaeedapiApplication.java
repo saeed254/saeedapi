42b1804e04d3d20c258a0875621ec4e24c25e38e
saeed254 <47712686+saeed254@users.noreply.github.com>
2023-03-13 06:26:10 -0700

    Add files via upload

---
+++ src/main/java/com/silicon/saeedapi/SaeedapiApplication.java
@@ -0,0 +1,13 @@
+package com.silicon.saeedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeedapiApplication.class, args);
+	}
+
+}
